a988820315c6621ab23d6951765a942a99e031f7
Serg Chernata <sergchernata@gmail.com>
2019-12-07 14:27:15 -0500

    initial server stub

---
+++ main.go
@@ -0,0 +1,24 @@
+package main
+
+import (
+	"./api"
+	"github.com/gorilla/mux"
+	"log"
+	"net/http"
+	"time"
+)
+
+func main() {
+	router := mux.NewRouter().StrictSlash(true)
+
+	router.HandleFunc("/webhook", api.Webhook).Methods("POST")
+
+	srv := &http.Server{
+		Handler:      router,
+		Addr:         "127.0.0.1:8090",
+		WriteTimeout: 15 * time.Second,
+		ReadTimeout:  15 * time.Second,
+	}
+
+	log.Fatal(srv.ListenAndServe())
+}
